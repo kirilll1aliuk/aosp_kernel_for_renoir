3765733fd64e5fa352621ff3717b7c9c13249541
kirilll1aliuk <132135447+kirilll1aliuk@users.noreply.github.com>
2024-09-01 16:56:58 +0300

    Update anykernel.sh

--- tools/ak3/anykernel.sh
+++ tools/ak3/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Kernel for Renoir by zeroXone
+kernel.string=Kernel for Renoir by llit
 do.devicecheck=1
 do.modules=0
 do.systemless=1
